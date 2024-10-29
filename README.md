6b16f95a4c593a5de5e371ae5ae21d0bb24d05f7
Brian Evans <brian.evans@optimalcompliance.com>
2024-10-29 12:24:46 +0000

    Add PSC descriptions
    Made the build script more generic to work on any file in the upstream repo.
    Added PSC descriptions to the export.
    Also put the existing constants under the Constants namespace in addition to existing exports.
    Can easily add other files in future when needed.

--- README.md
+++ README.md
@@ -7,14 +7,26 @@ Install https://www.npmjs.com/package/@oc-digital/ch-constants
 npm install @oc-digital/ch-constants
 ```
 
-Example usage:
+Example usage of main constants:
 ```js
 import { OfficerRole, SicDescriptions } from '@oc-digital/ch-constants';
 
 console.log(OfficerRole.director) // "Director"
 console.log(SicDescriptions["1440"]) // "Production of salt"
 ```
+These "top level" constants are also available from the `Constants` namespace, but exported at the top level for convenience and backward compatibility.
 
+## PSC descriptions (natures of control)
+Person with significant control descriptions are published under the `PscDescriptions` namespace.
+Example usage:
+```ts
+import { PscDescriptions } from '@oc-digital/ch-constants'
+
+console.log(PscDescriptions.ShortDescription["voting-rights-75-to-100-percent"])
+```
+
+## Package info
 Published as ES modules with Typescript definitions.
 
-Built automatically by pulling the yaml file from the Companies House repository directly.
+Built automatically by pulling the yaml files from the Companies House repository directly.
+Code generation script is open source, see `./build.ts`.
--- build.ts
+++ build.ts
@@ -1,32 +1,47 @@
 import {parse} from 'yaml'
-import {writeFile, mkdir, rm} from 'fs/promises'
 import {pascalCase} from 'change-case'
+import {mkdir, rm, writeFile} from 'node:fs/promises'
+import {basename} from 'node:path'
 
 // string transform to apply to the name of each constant mapping
 const transform = pascalCase
 
-const fileUrl = 'https://github.com/companieshouse/api-enumerations/raw/master/constants.yml'
-
-const contents = await fetch(fileUrl).then(res=>res.text())
-
-const constants: Record<string, Record<string,string>> = parse(contents)
+const files = ['constants.yml', 'psc_descriptions.yml']
 
 await rm('build', {recursive: true}) // clear previous build
 await mkdir('build', {recursive: true})
 
-for(const [snake_name, mapping] of Object.entries(constants)){
-    const name = transform(snake_name)
-    const fileContent = `export const ${name} = ${JSON.stringify(mapping, null, 2)}`
-    await writeFile(`build/${name}.js`, fileContent)
+const subdirs = []
 
-    const tsContent = Object.keys(mapping).length < 50 ? `export declare const ${name}: {
-${Object.entries(mapping).map(([key, value])=>`\t"${key}": ${typeof value};`).join('\n')}
+for (const file of files) {
+    const exportName = transform(basename(file, '.yml'))
+    const fileUrl = `https://raw.githubusercontent.com/companieshouse/api-enumerations/master/${file}`
+
+    const contents = await fetch(fileUrl).then(res => res.text())
+    const constants: Record<string, Record<string, string>> = parse(contents)
+
+    await mkdir(`build/${exportName}`, {recursive: true})
+
+    for (const [snake_name, mapping] of Object.entries(constants)) {
+        const name = transform(snake_name)
+        const filePath = `build/${exportName}/${name}`
+        const fileContent = `export const ${name} = ${JSON.stringify(mapping, null, 2)}`
+        await writeFile(filePath + '.js', fileContent)
+
+        const tsContent = Object.keys(mapping).length < 50 ? `export declare const ${name}: {
+${Object.entries(mapping).map(([key, value]) => `\t"${key}": ${typeof value};`).join('\n')}
 };` : `export declare const ${name}: Record<string, string>`
-    await writeFile(`build/${name}.d.ts`, tsContent)
+        await writeFile(`${filePath}.d.ts`, tsContent)
+    }
+
+    const indexContent = Object.keys(constants).map(name => `export { ${transform(name)} } from './${transform(name)}.js';`).join('\n')
+    await writeFile(`build/${exportName}/index.js`, indexContent)
+    await writeFile(`build/${exportName}/index.d.ts`, indexContent)
+    subdirs.push(exportName)
 }
 
-const indexContent = Object.keys(constants).map(name=>`export { ${transform(name)} } from './${transform(name)}.js';`).join('\n')
-await writeFile(`build/index.js`, indexContent)
+const indexContent = subdirs.map(name => `export * as ${name} from './${name}/index.js';`)
+    .concat(`export * from './Constants/index.js';`).join('\n')
 
-// const indexTsContent = Object.keys(constants).map(name=>`export { ${name} } from '${name}.mjs';`).join('\n')
+await writeFile(`build/index.js`, indexContent)
 await writeFile(`build/index.d.ts`, indexContent)
