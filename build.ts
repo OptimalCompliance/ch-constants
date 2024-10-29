38a044d2d3b31829980505544a463b41c2d9e82d
Brian Evans <brian.evans@optimalcompliance.com>
2024-10-29 13:00:01 +0000

    Normalise Unicode characters
    Convert hyphen and space variants to standard ASCII.

--- build.ts
+++ build.ts
@@ -2,6 +2,7 @@ import {parse} from 'yaml'
 import {pascalCase} from 'change-case'
 import {mkdir, rm, writeFile} from 'node:fs/promises'
 import {basename} from 'node:path'
+import {normalizeText} from "./normaliseText.ts";
 
 // string transform to apply to the name of each constant mapping
 const transform = pascalCase
@@ -15,7 +16,7 @@ const subdirs = []
 
 for (const file of files) {
     const exportName = transform(basename(file, '.yml'))
-    const fileUrl = `https://raw.githubusercontent.com/companieshouse/api-enumerations/master/${file}`
+    const fileUrl = `https://raw.githubusercontent.com/companieshouse/api-enumerations/refs/heads/master/${file}`
 
     const contents = await fetch(fileUrl).then(res => res.text())
     const constants: Record<string, Record<string, string>> = parse(contents)
@@ -25,7 +26,9 @@ for (const file of files) {
     for (const [snake_name, mapping] of Object.entries(constants)) {
         const name = transform(snake_name)
         const filePath = `build/${exportName}/${name}`
-        const fileContent = `export const ${name} = ${JSON.stringify(mapping, null, 2)}`
+        const normalisedMapping = Object.fromEntries(Object.entries(mapping)
+            .map(([key, value]) => [key, normalizeText(value)]))
+        const fileContent = `export const ${name} = ${JSON.stringify(normalisedMapping, null, 2)}`
         await writeFile(filePath + '.js', fileContent)
 
         const tsContent = Object.keys(mapping).length < 50 ? `export declare const ${name}: {
